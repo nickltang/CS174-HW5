1d6adf79e906e53a1fecf12c0e700ca2e6349ab9
eth4nd <howelldeguzman@gmail.com>
2022-12-04 00:13:03 -0800

    registration and login frontend/backend

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import Axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [usernameReg, setUsernameReg] = useState("");
+  const [passwordReg, setPasswordReg] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginStatus, setLoginStatus] = useState("");
+
+  const register = () => {
+    Axios.post("http://localhost8888/register", {
+      username: usernameReg,
+      password: passwordReg,
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+
+  const login = () => {
+    Axios.post("http://localhost8888/login", {
+      username: username,
+      password: password,
+    }).then((response) => {
+      if (response.data.message) {
+        setLoginStatus(response.data.message);
+      } else {
+        setLoginStatus(response.data[0].username);
+      }
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="registration">
+        <h1>Registration</h1>
+        <label>Username</label>
+        <input 
+          type="text" 
+          onChange={(e) => {
+            setUsernameReg(e.target.value);
+          }}
+        />
+        <label>Password</label>
+        <input 
+          type="text" 
+          onChange={(e) => {
+            setPasswordReg(e.target.value);
+          }}
+        />
+        <button onClick={register}> Register </button>
+      </div>
+      <div className="login">
+        <h1>Login</h1>
+        <input 
+          type="text" 
+          placeholder="Username..." 
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <input 
+          type="password" 
+          placeholder="Password..." 
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button onClick={login}> Login </button>
+      </div>
+      <h1>{loginStatus}</h1>
     </div>
   );
 }
